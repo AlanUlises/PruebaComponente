6996fbbbbea710450c5932886d42e433e4127caa
AlanUlises <amontes1@ucol.mx>
2017-03-29 09:59:11 -0600

    PruebaComponente

---
+++ PruebaComponente/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ProyectoPruebas;
+
+namespace PruebaComponente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Burbuja burbu = new Burbuja();
+            int[] arr = new int[5];
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Ingresa un numero: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());                
+            }
+
+            arr = burbu.ordenar(arr);
+
+            for(int i = 0; i < arr.Length; i++)
+                Console.WriteLine(arr[i]);
+
+            Console.Read();
+        }
+    }
+}
